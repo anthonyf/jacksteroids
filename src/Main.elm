796d7caa6a93727eb5a72d1278d3b7d00d2d7569
Anthony Fairchild <fairchild.anthony@gmail.com>
2026-06-28 19:04:48 -0400

    Add controllable ship

--- src/Main.elm
+++ src/Main.elm
@@ -3,17 +3,44 @@ module Main exposing (main)
 import Browser
 import Browser.Events
 import Html exposing (Html)
-import Svg exposing (Svg, rect, svg)
-import Svg.Attributes exposing (fill, height, viewBox, width)
+import Json.Decode as Decode
+import Svg exposing (Svg, polygon, rect, svg)
+import Svg.Attributes exposing (fill, height, points, stroke, strokeWidth, transform, viewBox, width)
 
 
 type alias Model =
     { elapsedMs : Float
+    , ship : Ship
+    , controls : Controls
+    }
+
+
+type alias Ship =
+    { position : Vec2
+    , velocity : Vec2
+    , thrust : Vec2
+    , heading : Float
+    , angularVelocity : Float
+    , collisionBoundary : Float
+    }
+
+
+type alias Vec2 =
+    { x : Float
+    , y : Float
+    }
+
+
+type alias Controls =
+    { thrusting : Bool
+    , turningLeft : Bool
+    , turningRight : Bool
     }
 
 
 type Msg
     = Tick Float
+    | KeyChanged String Bool
 
 
 main : Program () Model Msg
@@ -28,7 +55,10 @@ main =
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { elapsedMs = 0 }
+    ( { elapsedMs = 0
+      , ship = initialShip
+      , controls = initialControls
+      }
     , Cmd.none
     )
 
@@ -37,24 +67,38 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Tick deltaMs ->
-            ( { model | elapsedMs = model.elapsedMs + deltaMs }
+            ( { model
+                | elapsedMs = model.elapsedMs + deltaMs
+                , ship = updateShip deltaMs model.controls model.ship
+              }
+            , Cmd.none
+            )
+
+        KeyChanged key isPressed ->
+            ( { model | controls = updateControls key isPressed model.controls }
             , Cmd.none
             )
 
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
-    Browser.Events.onAnimationFrameDelta Tick
+    Sub.batch
+        [ Browser.Events.onAnimationFrameDelta Tick
+        , Browser.Events.onKeyDown (Decode.map (\key -> KeyChanged key True) keyDecoder)
+        , Browser.Events.onKeyUp (Decode.map (\key -> KeyChanged key False) keyDecoder)
+        ]
 
 
 view : Model -> Html Msg
-view _ =
+view model =
     svg
         [ width "100vw"
         , height "100vh"
         , viewBox "0 0 800 600"
         ]
-        [ background ]
+        [ background
+        , viewShip model.ship
+        ]
 
 
 background : Svg Msg
@@ -65,3 +109,180 @@ background =
         , fill "#000"
         ]
         []
+
+
+initialShip : Ship
+initialShip =
+    { position = { x = 400, y = 300 }
+    , velocity = zero
+    , thrust = zero
+    , heading = 0
+    , angularVelocity = 0
+    , collisionBoundary = 18
+    }
+
+
+initialControls : Controls
+initialControls =
+    { thrusting = False
+    , turningLeft = False
+    , turningRight = False
+    }
+
+
+keyDecoder : Decode.Decoder String
+keyDecoder =
+    Decode.field "key" Decode.string
+
+
+updateControls : String -> Bool -> Controls -> Controls
+updateControls key isPressed controls =
+    case key of
+        "ArrowUp" ->
+            { controls | thrusting = isPressed }
+
+        "ArrowLeft" ->
+            { controls | turningLeft = isPressed }
+
+        "ArrowRight" ->
+            { controls | turningRight = isPressed }
+
+        _ ->
+            controls
+
+
+updateShip : Float -> Controls -> Ship -> Ship
+updateShip deltaMs controls ship =
+    let
+        deltaSeconds =
+            deltaMs / 1000
+
+        angularVelocity =
+            rotationFromControls controls
+
+        heading =
+            ship.heading + angularVelocity * deltaSeconds
+
+        thrust =
+            if controls.thrusting then
+                thrustFromHeading heading
+
+            else
+                zero
+
+        velocity =
+            add ship.velocity (scale deltaSeconds thrust)
+
+        position =
+            wrapPosition (add ship.position (scale deltaSeconds velocity))
+    in
+    { ship
+        | position = position
+        , velocity = velocity
+        , thrust = thrust
+        , heading = heading
+        , angularVelocity = angularVelocity
+    }
+
+
+rotationFromControls : Controls -> Float
+rotationFromControls controls =
+    case ( controls.turningLeft, controls.turningRight ) of
+        ( True, False ) ->
+            -turnSpeed
+
+        ( False, True ) ->
+            turnSpeed
+
+        _ ->
+            0
+
+
+thrustFromHeading : Float -> Vec2
+thrustFromHeading heading =
+    { x = sin heading * thrustPower
+    , y = -(cos heading) * thrustPower
+    }
+
+
+viewShip : Ship -> Svg Msg
+viewShip ship =
+    polygon
+        [ points "0,-24 13,14 -13,14"
+        , fill "#000"
+        , stroke "#fff"
+        , strokeWidth "2"
+        , transform
+            ("translate("
+                ++ String.fromFloat ship.position.x
+                ++ " "
+                ++ String.fromFloat ship.position.y
+                ++ ") rotate("
+                ++ String.fromFloat (radiansToDegrees ship.heading)
+                ++ ")"
+            )
+        ]
+        []
+
+
+wrapPosition : Vec2 -> Vec2
+wrapPosition position =
+    { x = wrap 0 playfieldWidth position.x
+    , y = wrap 0 playfieldHeight position.y
+    }
+
+
+wrap : Float -> Float -> Float -> Float
+wrap lower upper value =
+    if value < lower then
+        upper
+
+    else if value > upper then
+        lower
+
+    else
+        value
+
+
+add : Vec2 -> Vec2 -> Vec2
+add a b =
+    { x = a.x + b.x
+    , y = a.y + b.y
+    }
+
+
+scale : Float -> Vec2 -> Vec2
+scale scalar vector =
+    { x = scalar * vector.x
+    , y = scalar * vector.y
+    }
+
+
+radiansToDegrees : Float -> Float
+radiansToDegrees radians =
+    radians * 180 / pi
+
+
+zero : Vec2
+zero =
+    { x = 0, y = 0 }
+
+
+playfieldWidth : Float
+playfieldWidth =
+    800
+
+
+playfieldHeight : Float
+playfieldHeight =
+    600
+
+
+thrustPower : Float
+thrustPower =
+    260
+
+
+turnSpeed : Float
+turnSpeed =
+    4
